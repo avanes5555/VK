f82294f87e57c3b23b10afbd73b2f50ff6738927
avanes5555@gmail.com <avanes5555@gmail.com>
2019-06-03 16:10:36 +0300

    02.06.19

--- VK/Controllers/VKLoginController.swift
+++ VK/Controllers/VKLoginController.swift
@@ -10,8 +10,12 @@ import UIKit
 import WebKit
 
 class VKLoginController: UIViewController {
+    
+    @IBOutlet var webView: WKWebView!
+    
 
-    @IBOutlet weak var webView: WKWebView! {
+    //@IBOutlet weak var webView: WKWebView!
+        {
         didSet {
             webView.navigationDelegate = self
         }
@@ -40,5 +44,21 @@ class VKLoginController: UIViewController {
 
 
 extension VKLoginController: WKNavigationDelegate {
-    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard let url = navigationResponse.response.url,
+        url.path == "/blank.html",
+            let fragment = url.fragment else {decisionHandler(.allow); return}
+        let params = fragment.commonPrefix(with: "&")
+        .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        print (params)
+    }
 }
